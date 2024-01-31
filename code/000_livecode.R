2eac37d22152b7cf1660ec8e7930e1b93f1b3ac9
Christian John <cjohn@ucdavis.edu>
2024-01-31 14:39:38 -0800

    pushed livecode update

--- code/000_livecode.R
+++ code/000_livecode.R
@@ -5,7 +5,7 @@ library(sf)
 library(terra)
 library(tidyverse)
 
-#### Import data
+#### Import data; vector ----
 moo = read_sf("data/moorea_outline.shp")
 moo
 plot(moo)
@@ -13,6 +13,22 @@ moo
 
 fieldSites_df = read_csv("data/fieldSiteLocations.csv")
 
-fieldSites_df %>%
-  st_as_sf(coords = c("x", "y"))
+fieldSites_sf =  fieldSites_df %>%
+  st_as_sf(coords = c("x", "y"),
+           crs = 4326)
+fieldSites_sf
+
+
+#### Import data, raster ----
+sst = terra::rast("data/MODmed.tif")
+sst
+plot(sst)
+
+hill = rast("data/hillshade.tif")
+hill
+plot(hill)
+
+
+#### Wrangling ---
+
 
