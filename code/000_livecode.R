a5c4814ddb825315b79867b3d38fb13becd0325e
Christian John <cjohn@ucdavis.edu>
2024-01-31 14:30:14 -0800

    pushed livecode update

--- code/000_livecode.R
+++ code/000_livecode.R
@@ -1,5 +1,14 @@
 ## Follow along here.
 
+#### Load pacakges
 library(sf)
 library(terra)
 library(tidyverse)
+
+#### Import data
+moo = read_sf("data/moorea_outline.shp")
+moo
+plot(moo)
+moo
+
+fieldSites_df = read_csv("data/fieldSiteLocations.csv")
