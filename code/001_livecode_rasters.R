dc5b71091000afa0ab2aaddb0985dad849975d5f
Christian John <cjohn@ucdavis.edu>
2024-03-11 11:10:41 -0700

    pushed livecode update

--- code/001_livecode_rasters.R
+++ code/001_livecode_rasters.R
@@ -4,3 +4,12 @@
 ## Loading rasters, inspecting raster contents and structure
 ## Dealing with annoyances
 ## 
+
+#### Load packages -----
+library(terra)
+library(sf)
+library(tidyverse)
+
+#### load data ----
+fp = read_sf("data/FrenchPoly.shp")
+fp
